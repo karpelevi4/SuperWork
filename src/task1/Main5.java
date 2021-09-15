62b51fae8e621f56852da6f159f2c2a31c221d1c
qt-bb <nastyan.nt8633@gmail.com>
2021-09-15 18:41:04 +0300

    Harmonic series task completed.

--- src/task1/Main5.java
+++ src/task1/Main5.java
@@ -1,4 +1,10 @@
 package task1;
 
+import java.text.DecimalFormat;
+
 public class Main5 {
+    public static void main(String[] args) {
+        for (double i = 1; i <= 10; i++)
+            System.out.print(new DecimalFormat("#.####").format(1 / i) + " ");
+    }
 }
