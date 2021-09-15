19ecf3308b1425aab8a002598738c27b92b6590c
qt-bb <nastyan.nt8633@gmail.com>
2021-09-15 18:12:27 +0300

    Deleted unnecessary stuff.

--- src/task1/Main3.java
+++ src/task1/Main3.java
@@ -2,24 +2,6 @@ package task1;
 import java.util.Scanner;
 
 public class Main3 {
-    //задание 1
-    //метод для получения суммы чисел массива
-    public static int sum(int[] a) {
-        int s = 0;
-        for (int i: a) s += i;
-        return s;
-    }
-
-    //задание 5
-    //метод, вычисляющий факториал числа с помощью цикла
-    public static int factorial(int n) {
-        int f = 1;
-        for (int i = n; i!=1; i--) {
-            f *=i;
-        }
-        return f;
-    }
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Введите массив из целых чисел.");
