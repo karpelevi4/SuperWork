92e2bf5c6a1288aaa707a63d1482e65244ce18a7
qt-bb <nastyan.nt8633@gmail.com>
2021-09-18 16:03:09 +0300

    Changing input form.

--- src/task1/Main3.java
+++ src/task1/Main3.java
@@ -1,30 +1,46 @@
 package task1;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Введите массив из целых чисел.");
-        String m = s.nextLine();
-        String[] numbers = m.split(" ");
+        System.out.println("Введите массив из целых чисел. (Завершение - Ctrl+D)");
+
+        //Заполнение списка числами, вводимыми пользователем.
+        List<Integer> nList = new ArrayList<Integer>();
+        while (s.hasNextInt()) {
+            nList.add(s.nextInt());
+        }
+
+        //Создание массива из списка
+        int[] nArray = new int[nList.size()];
+        for(int i = 0; i < nList.size(); i++) nArray[i] = nList.get(i);
+
+        //Цикл for
         int sum = 0;
-        for (String i : numbers) {
-            sum += Integer.parseInt(i);
+        for (int i : nArray) {
+            sum += i;
         }
         System.out.println("Сумма элементов с помощью цикла for: " + sum);
+
+        //Цикл while
         sum = 0;
-        int l = numbers.length - 1;
+        int l = nArray.length - 1;
         while (l != -1) {
-            sum += Integer.parseInt(numbers[l]);
+            sum += nArray[l];
             l--;
         }
         System.out.println("Сумма элементов с помощью цикла while: " + sum);
+
+        //Цикл do while
         sum = 0;
         l++;
         do {
-            sum += Integer.parseInt(numbers[l]);
+            sum += nArray[l];
             l++;
-        } while (l < numbers.length);
+        } while (l < nArray.length);
         System.out.println("Сумма элементов с помощью цикла do while: " + sum);
     }
 }
