27967f5fa855d4db5e1acc73988af7a7f07a0283
qt-bb <nastyan.nt8633@gmail.com>
2021-09-18 15:18:03 +0300

    Editing output format settings.

--- src/task1/Main5.java
+++ src/task1/Main5.java
@@ -5,6 +5,6 @@ import java.text.DecimalFormat;
 public class Main5 {
     public static void main(String[] args) {
         for (double i = 1; i <= 10; i++)
-            System.out.printf("%-8s",new DecimalFormat("#.####").format(1 / i));
+            System.out.printf("%-8s",new DecimalFormat("0.000").format(1 / i));
     }
 }
