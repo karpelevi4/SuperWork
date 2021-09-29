3b8288fe6f48ade1c3cc5703514a78db42c82f7c
qt-bb <nastyan.nt8633@gmail.com>
2021-09-29 14:00:38 +0300

    Changing test values.

--- src/task5/TestClass.java
+++ src/task5/TestClass.java
@@ -3,7 +3,7 @@ package task5;
 public class TestClass {
     public static void main(String[] args){
         Plate plate_one = new Plate("стекло", 549, 22);
-        Cup cup_one = new Cup("керамика", 499, 24);
+        Cup cup_one = new Cup("керамика", 499, 350);
 
         plate_one.displayInfo();
         System.out.println();
