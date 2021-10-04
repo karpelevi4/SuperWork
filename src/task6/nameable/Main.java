2b9a087d6efd4457c83d2b56220e7df82e89bd0d
qt-bb <nastyan.nt8633@gmail.com>
2021-10-04 20:45:13 +0300

    fixing String[] args

--- src/task6/nameable/Main.java
+++ src/task6/nameable/Main.java
@@ -1,7 +1,7 @@
 package task6.nameable;
 
 public class Main {
-    public static void main (String args[]) {
+    public static void main (String[] args) {
         Animal cat = new Animal("John", 3);
         System.out.println("This cat's name is " + cat.getName());
     }
