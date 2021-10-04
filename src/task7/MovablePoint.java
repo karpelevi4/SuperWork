bdda5ce727e40ccbe4c2fefb819b64319fba08b8
qt-bb <nastyan.nt8633@gmail.com>
2021-10-04 19:20:33 +0300

    /

--- src/task7/MovablePoint.java
+++ src/task7/MovablePoint.java
@@ -1,23 +1,46 @@
 package task7;
 
 public class MovablePoint implements Movable{
+
+    int x; //package-private (no explicit modifier)
+    int y;
+    int xSpeed;
+    int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.xSpeed = xSpeed;
+        this.y = y;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + x +
+                ", y=" + y +
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+
     @Override
     public void moveUp() {
-
+        this.y++;
     }
 
     @Override
     public void moveDown() {
-
+        this.y--;
     }
 
     @Override
     public void moveLeft() {
-
+        this.x--;
     }
 
     @Override
     public void moveRight() {
-
+        this.x++;
     }
 }
--- src/task7/Square.java
+++ src/task7/Square.java
@@ -4,23 +4,23 @@ public class Square extends Rectangle {
 
     public Square() {
         super();
-    };
+    }
 
     public Square(double side) {
         this.width = side;
         this.length = side;
-    };
+    }
 
     public Square(double side, String color, boolean filled) {
         super(side, side, color, filled);
-    };
+    }
 
-    public double getSide() {return width;};
+    public double getSide() {return width;}
 
     public  void setSide(double side) {
         this.width = side;
         this.length = side;
-    };
+    }
 
     @Override
     public void setWidth(double width) {
