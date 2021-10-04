7e9f80c2c3b787dbcd330534c27128961d812e56
qt-bb <nastyan.nt8633@gmail.com>
2021-10-04 15:57:19 +0300

    Editing Circle class.

--- src/task7/Circle.java
+++ src/task7/Circle.java
@@ -7,11 +7,13 @@ public class Circle extends Shape {
     public Circle() {}
 
     public Circle(double radius) {
+        super();
         this.radius = radius;
     }
 
     public Circle(double radius, String color, boolean filled) {
-
+        super(color, filled);
+        this.radius = radius;
     }
 
     public double getRadius() {
@@ -36,6 +38,8 @@ public class Circle extends Shape {
     public String toString() {
         return "Circle{" +
                 "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
                 '}';
     }
 }
