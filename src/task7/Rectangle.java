512a41256bf64d81d938c575e6ad588721ffb033
qt-bb <nastyan.nt8633@gmail.com>
2021-10-04 14:19:03 +0300

    Task 7 half way done.

--- src/task7/Rectangle.java
+++ src/task7/Rectangle.java
@@ -1,4 +1,47 @@
 package task7;
 
-public class Rectangle {
+public class Rectangle extends Shape{
+
+    protected double width;
+    protected double length;
+
+    public Rectangle() {};
+
+    public Rectangle(double width, double length) {};
+
+    public Rectangle(double width, double length, String color, boolean filled) {};
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width*length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*(width+length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                '}';
+    }
 }
--- src/task7/Square.java
+++ src/task7/Square.java
@@ -1,4 +1,29 @@
 package task7;
 
-public class Square {
+public class Square extends Rectangle {
+
+    public Square() {};
+
+    public Square(double side) {};
+
+    public Square(double side, String color, boolean filled) {};
+
+    public double getSide() {return 1;};
+
+    public  void setSide(double side) {};
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        super.setLength(length);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
