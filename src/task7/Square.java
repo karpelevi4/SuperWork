17624500dd3083193d4c84db90e86a49eb409def
qt-bb <nastyan.nt8633@gmail.com>
2021-10-04 16:06:09 +0300

    Editing Square class.

--- src/task7/Square.java
+++ src/task7/Square.java
@@ -2,15 +2,25 @@ package task7;
 
 public class Square extends Rectangle {
 
-    public Square() {};
+    public Square() {
+        super();
+    };
 
-    public Square(double side) {};
+    public Square(double side) {
+        this.width = side;
+        this.length = side;
+    };
 
-    public Square(double side, String color, boolean filled) {};
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    };
 
-    public double getSide() {return 1;};
+    public double getSide() {return width;};
 
-    public  void setSide(double side) {};
+    public  void setSide(double side) {
+        this.width = side;
+        this.length = side;
+    };
 
     @Override
     public void setWidth(double width) {
