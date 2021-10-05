47cf59611d0a9b51840567e912ed5e4ca6f0160b
qt-bb <nastyan.nt8633@gmail.com>
2021-10-05 11:38:56 +0300

    Making task 4.

---
+++ src/task4/ball/TestBall.java
@@ -0,0 +1,8 @@
+package task4.ball;
+
+public class TestBall {
+    public static void main(String[] args) {
+        Ball standartBall = new Ball();
+        System.out.println(standartBall);
+    }
+}
