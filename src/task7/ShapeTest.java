b21b0c80c1d3a536ace292fd697035479c17b9de
qt-bb <nastyan.nt8633@gmail.com>
2021-10-05 10:28:40 +0300

    Added comment about late binding.

--- src/task7/ShapeTest.java
+++ src/task7/ShapeTest.java
@@ -4,6 +4,8 @@ public class ShapeTest {
     public static void main(String[] args) {
         Shape s1 = new Circle(5.5, "RED", false); // Upcast Circle to Shape
         System.out.println(s1); // which version?
+                                //виртуальные методы разрешаются при помощи динамического (позднего) связывания
+                                //динамическая привязка использует объект для разрешения привязки
         System.out.println(s1.getArea()); // which version?
         System.out.println(s1.getPerimeter()); // which version?
         System.out.println(s1.getColor());
