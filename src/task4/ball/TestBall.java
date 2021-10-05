6a19000a0ed623684865ca7c218554434ac8472e
qt-bb <nastyan.nt8633@gmail.com>
2021-10-05 11:43:26 +0300

    Task 4 done.

--- src/task4/ball/TestBall.java
+++ src/task4/ball/TestBall.java
@@ -4,5 +4,14 @@ public class TestBall {
     public static void main(String[] args) {
         Ball standartBall = new Ball();
         System.out.println(standartBall);
+        standartBall.move(6, 17.5);
+        System.out.println(standartBall);
+        Ball secondBall = new Ball(15, 33.9);
+        System.out.println(secondBall.getX() + " " +secondBall.getY());
+        secondBall.setX(45);
+        secondBall.setY(71.2);
+        System.out.println(secondBall.getX() + " " +secondBall.getY());
+        secondBall.setXY(0, 0);
+        System.out.println(secondBall.getX() + " " +secondBall.getY());
     }
 }
