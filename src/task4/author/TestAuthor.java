225e236515fe6aeddc8710a470d80843d646ba2e
qt-bb <nastyan.nt8633@gmail.com>
2021-10-05 11:39:44 +0300

    removing unnecessary toString()

--- src/task4/author/TestAuthor.java
+++ src/task4/author/TestAuthor.java
@@ -3,6 +3,6 @@ package task4.author;
 public class TestAuthor {
     public static void  main(String[] args) {
         Author a = new Author("Алексей Иванов", "a_ivanov@mail.com", 'м');
-        System.out.println(a.toString());
+        System.out.println(a);
     }
 }
