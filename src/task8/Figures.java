5b22cdd64faa2a41aeb6523f9ec1c6cbbcf17080
qt-bb <nastyan.nt8633@gmail.com>
2021-10-07 11:35:42 +0300

    removed unused imports and started task9

--- src/task8/Figures.java
+++ src/task8/Figures.java
@@ -10,8 +10,6 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
 public class Figures extends Application {
