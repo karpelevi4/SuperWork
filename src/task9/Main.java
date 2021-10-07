b557d63fc2a8c8f5b1d23ae38db7821007952bb5
qt-bb <nastyan.nt8633@gmail.com>
2021-10-07 12:19:24 +0300

    new labels and trying lambda functions

--- src/task9/Main.java
+++ src/task9/Main.java
@@ -2,6 +2,7 @@ package task9;
 
 import javafx.application.Application;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -15,12 +16,38 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) {
+        int milanPoints = 0;
+        int madridPoints = 0;
+
+
         Button milanButton = new Button("Милан");
 
         Button madridButton = new Button("Мадрид");
-        madridButton.setLayoutX(200); //размещение кнопки "Мадрид"
+        madridButton.setLayoutX(60);
 
-        Group root = new Group(madridButton, milanButton);
+        Label resaultLabel = new Label("Result: " + milanPoints + " X " + madridPoints);
+        resaultLabel.setLayoutY(50);
+
+        Label lastScorerLabel = new Label("Last Scorer: N/A");
+        lastScorerLabel.setLayoutY(100);
+
+        Label winnerLabel = new Label("Winner: DRAW");
+        winnerLabel.setLayoutY(150);
+
+        milanButton.setOnAction(value ->  {
+
+            resaultLabel.setText("Result: " + (milanPoints + 1) + " X " + madridPoints);
+            lastScorerLabel.setText("Last Scorer: Milan");
+        });
+
+        madridButton.setOnAction(value ->  {
+
+            resaultLabel.setText("Result: " + milanPoints + " X " + (madridPoints + 1));
+            lastScorerLabel.setText("Last Scorer: Madrid");
+        });
+
+        Group root = new Group(madridButton, milanButton, resaultLabel,
+                lastScorerLabel, winnerLabel);
 
 
         root.getChildren().addAll();
