1870f4273652404c2dcab2534e8f69fbc351305f
qt-bb <nastyan.nt8633@gmail.com>
2021-10-08 13:23:31 +0300

    book class for task2 is done

--- src/task2/Book.java
+++ src/task2/Book.java
@@ -1,4 +1,64 @@
 package task2;
 
 public class Book {
+
+    private String name;
+    private int price;
+    private String author;
+    private boolean read;
+
+    public Book(String author, String name, int price) {
+        this.author = author;
+        this.name = name;
+        this.price = price;
+        this.read = false;
+    }
+
+    public Book(String name, int price) {
+        this.author = "Unknown";
+        this.name = name;
+        this.price = price;
+        this.read = false;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void markAsRead() {
+        this.read = true;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    @Override
+    public String toString() {
+        return "Book is called " + name +
+                ", it's price is " + price +
+                ", it is written by " + author +
+                ", you have" + (this.isRead()?" ":" not ") +
+                "read it yet.";
+    }
 }
