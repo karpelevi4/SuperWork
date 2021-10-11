38ba8bc3e093303b9239189aae40e178ed29c88d
qt-bb <nastyan.nt8633@gmail.com>
2021-10-11 20:25:04 +0300

    dialog window created

--- src/task11/sides/SidesTest.java
+++ src/task11/sides/SidesTest.java
@@ -1,10 +1,11 @@
 package task11.sides;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Alert;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -18,27 +19,38 @@ public class SidesTest extends Application {
 	public void start(Stage stage) {
 		
 		Canvas canvas = new Canvas(800,600);
-		Pane root = new Pane(canvas);
-		Scene scene = new Scene(root );
+		double cWidth = canvas.getWidth();
+		double cHeight = canvas.getHeight();
+		Group root = new Group(canvas);
+		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.setTitle("Sides");
 		
 		GraphicsContext g = canvas.getGraphicsContext2D();
 		
 		g.setFill(Color.web("#b5cef7"));
-		g.fillRect(0,0,canvas.getWidth()/2, canvas.getHeight()/2);
+		g.fillRect(cWidth/3, 0, cWidth/3, cHeight/3);
 		
 		g.setFill(Color.web("#b5f7c6"));
-		g.fillRect(canvas.getWidth()/2,0,canvas.getWidth(), canvas.getHeight()/2);
+		g.fillRect(0, cHeight/3, cWidth/3, cHeight/3);
 		
 		g.setFill(Color.web("#f7dfb5"));
-		g.fillRect(0,canvas.getHeight()/2,canvas.getWidth()/2, canvas.getWidth());
+		g.fillRect(cWidth/3, cHeight*2/3, cWidth/3, cHeight/3);
 		
 		g.setFill(Color.web("#e3b5f7"));
-		g.fillRect(canvas.getWidth()/2,canvas.getHeight()/2,canvas.getWidth(), canvas.getHeight());
+		g.fillRect(cWidth*2/3, cHeight/3, cWidth/3, cHeight/3);
 		
-		g.setFill(Color.web("#fff"));
-		g.fillOval(canvas.getWidth()/4,canvas.getHeight()/4,canvas.getWidth()/2, canvas.getHeight()/2);
+		g.setFill(Color.web("#e9f7b5"));
+		g.fillRect(cWidth/3, cHeight/3, cWidth/3, cHeight/3);
+		
+		canvas.setOnMouseEntered(e -> {
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			alert.setTitle("Мышь вошла в определённую область");
+			alert.setHeaderText(null);
+			alert.setGraphic(null);
+			alert.setContentText("Добро пожаловать");
+			alert.showAndWait();
+		});
 		
 		stage.show();
 	}
