2c365c4c4d08b16373418177a7fd4756c0ad52dc
qt-bb <nastyan.nt8633@gmail.com>
2021-10-11 21:48:18 +0300

    tabs

--- src/task11/textArea/TextAreaTest.java
+++ src/task11/textArea/TextAreaTest.java
@@ -7,42 +7,43 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class TextAreaTest extends Application {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        launch(args);
-    }
+		launch(args);
+	}
 
-    public void addMenuToBar(MenuBar menuBar, String menuName, String firstChoice, String secondChoice, String thirdChoice) {
-        Menu menu = new Menu(menuName);
-        MenuItem firstItem = new MenuItem(firstChoice);
-        MenuItem secondItem = new MenuItem(secondChoice);
-        MenuItem thirdItem = new MenuItem(thirdChoice);
-        menu.getItems().addAll(firstItem, secondItem, thirdItem);
-        menuBar.getMenus().add(menu);
-    }
+	public void addMenuToBar(MenuBar menuBar, String menuName, String firstChoice, String secondChoice, String thirdChoice) {
+		Menu menu = new Menu(menuName);
+		MenuItem firstItem = new MenuItem(firstChoice);
+		MenuItem secondItem = new MenuItem(secondChoice);
+		MenuItem thirdItem = new MenuItem(thirdChoice);
+		menu.getItems().addAll(firstItem, secondItem, thirdItem);
+		menuBar.getMenus().add(menu);
+	}
 
-    @Override
-    public void start(Stage stage) {
+	@Override
+	public void start(Stage stage) {
 
-        MenuBar menuBar = new MenuBar();
-        addMenuToBar(menuBar, "Цвет", "Красный", "Синий", "Чёрный");
-        addMenuToBar(menuBar, "Шрифт", "Verdana", "Helvetica", "Impact");
+		MenuBar menuBar = new MenuBar();
+		addMenuToBar(menuBar, "Цвет", "Красный", "Синий", "Чёрный");
+		addMenuToBar(menuBar, "Шрифт", "Verdana", "Helvetica", "Impact");
 
-        BorderPane top = new BorderPane();
-        top.setTop(menuBar);
+		BorderPane top = new BorderPane();
+		top.setTop(menuBar);
+		
+		
 
-        Group root = new Group(top);
+		Group root = new Group(top);
 
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Sides");
-        stage.setWidth(800);
-        stage.setHeight(800);
-        stage.show();
-    }
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("Text area");
+		stage.setWidth(800);
+		stage.setHeight(800);
+		stage.show();
+	}
 }
